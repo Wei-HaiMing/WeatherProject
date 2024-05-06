4853158bca04dc34a0f71280bd589f6b4acd6357
gabe-tommi <gabetgal2112@gmail.com>
2024-05-06 14:01:07 -0700

    Redirect function and name display
    Other future API scraped code is commented out

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 """
 Authors: Armando, Lily, Eduardo, Gabe
 """
-from flask import Flask, render_template, request
+from flask import  Flask, render_template, request, redirect
 from PIL import Image
 from flask_bootstrap import Bootstrap5
 import random
@@ -17,35 +17,39 @@ my_key = "77b4df041ddc47e427d8799ea237cb80"
 #     "appid": my_key,
 #     "limit": 1
 # }
-city_name = "Salinas"
-limit = 1
-
-geo_endpoint = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit={limit}&appid={my_key}"
-
-try:
-    r = requests.get(geo_endpoint)
-    data = r.json()
-    pprint(data)
-except:
-    print("geo fail, please try again")
-
-weather_endpoint = f"https://api.openweathermap.org/data/2.5/weather?lat={data[0]['lat']}&lon={data[0]['lon']}&appid={my_key}"
-
-try:
-    r = requests.get(weather_endpoint)
-    data = r.json()
-    pprint(data)
-except:
-    print("weather fail, please try again")
-
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
-@app.route('/')
-def homepage():
-    return render_template('Weather_Details.html')
 
-# @app.route('/submit', methods=['POST'])
-# def link():
-#     textinput = request.form['textinput']
-#     return render_template('Weather_Deatils.html', textinput = textinput)
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    global city_name
+    city_name = request.form.get('citytxtbox')
+    return render_template('home.html', city_name = city_name)
+
+
+@app.route('/details')
+def link():
+    city_name = request.args.get('citytxtbox')
+
+    # limit = 1
+
+    # geo_endpoint = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit={limit}&appid={my_key}"
+
+    # try:
+    #     r = requests.get(geo_endpoint)
+    #     data = r.json()
+    #     pprint(data)
+    # except:
+    #     print("geo fail, please try again")
+
+    # weather_endpoint = f"https://api.openweathermap.org/data/2.5/weather?lat={data[0]['lat']}&lon={data[0]['lon']}&appid={my_key}"
+
+    # try:
+    #     r = requests.get(weather_endpoint)
+    #     data = r.json()
+    #     pprint(data)
+    # except:
+    #     print("weather fail, please try again")
+
+    return render_template('Weather_Details.html', city_name = city_name)
